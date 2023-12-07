4ffd18f18e7f26761eb54e39d2274385a394175c
ujwal <ujwalkumar.am@gmail.com>
2023-12-07 22:11:04 +0530

    Hi

---
+++ app/src/main/java/com/example/myapplication/BlankFragment.kt
@@ -0,0 +1,19 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+
+
+
+
+class BlankFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+
+
+
+}
