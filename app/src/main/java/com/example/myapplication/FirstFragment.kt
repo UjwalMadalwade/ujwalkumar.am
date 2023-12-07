4485d7a2a9b630d158d876e911f2a4862bd2b769
ujwal <ujwalkumar.am@gmail.com>
2023-12-07 22:26:08 +0530

    new project

---
+++ app/src/main/java/com/example/myapplication/FirstFragment.kt
@@ -0,0 +1,37 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import com.example.myapplication.databinding.FragmentFirstBinding
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class FirstFragment : Fragment() {
+
+    private var _binding: FragmentFirstBinding? = null
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        binding.buttonFirst.setOnClickListener {
+            findNavController().navigate(R.id.action_firstFragment2_to_secondFragment)
+
+        }
+        return binding.root
+    }
+
+}
+
+
